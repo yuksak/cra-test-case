3ac0aa49e57a0a11e34e45b437ae026568bb3e72
yuksak <gulamaier@gmail.com>
2022-10-13 21:51:21 +0500

    init: start cra-test-case task

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      <header>Header</header>
+      <Routes>
+        <Route
+          path="*"
+          element={<h1>404 Error. Could not find such page!</h1>}
+        />
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/about-us" element={<h1>About Us</h1>} />
+        <Route path="/products" element={<h1>Products</h1>} />
+        <Route path="/sale" element={<h1>Sale</h1>} />
+      </Routes>
+      <footer>Footer</footer>
+    </>
+  );
+};
+
+export default App;
