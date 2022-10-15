8eaab65d992473b5d4acad682e98d2eb539d85ef
yuksak <gulamaier@gmail.com>
2022-10-15 07:49:44 +0500

    refactor: change structure of the style

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,15 +1,24 @@
 import React from "react";
-import { FiArrowUp, FiFacebook, FiInstagram, FiTwitter } from "react-icons/fi";
 import { Link } from "react-router-dom";
-import { logo } from "../../images/logos";
+
 import styles from "./index.module.scss";
+
+import { FiArrowUp, FiFacebook, FiInstagram, FiTwitter } from "react-icons/fi";
 
 const Footer = () => {
   return (
     <div className={styles.footer}>
       <div className={styles.footer__main}>
         <div className={styles.footer__socials}>
-          <img src={logo} alt="logo" />
+          <svg
+            className={styles.footer__logo}
+            viewBox="0 0 49 48"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M13 9L0 22V0H13V9Z" />
+            <path d="M13.5 48H0V31L30 1H48L13.5 35.5V48Z" />
+            <path d="M29.5 29L20.5 38L29.5 47.5H48L29.5 29Z" />
+          </svg>
           <p>
             Our shop is the best choice
             <br /> for buying footwear.
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
-import { logo } from "../../images/logos";
-
 import styles from "./index.module.scss";
 
 import { FiSearch, FiUser, FiShoppingBag } from "react-icons/fi";
@@ -19,7 +17,11 @@ const Header = () => {
   return (
     <div className={styles.header}>
       <div className={styles.header__logo}>
-        <img src={logo} alt="logo" />
+        <svg viewBox="0 0 49 48" xmlns="http://www.w3.org/2000/svg">
+          <path d="M13 9L0 22V0H13V9Z" />
+          <path d="M13.5 48H0V31L30 1H48L13.5 35.5V48Z" />
+          <path d="M29.5 29L20.5 38L29.5 47.5H48L29.5 29Z" />
+        </svg>
       </div>
 
       <nav
